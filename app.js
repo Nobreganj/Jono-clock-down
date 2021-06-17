3f75adcb6c37180bed3a8a4cab4370408d1adab7
Nobreganj <57805973+Nobreganj@users.noreply.github.com>
2021-06-17 01:54:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const seconds = document.getElementById('seconds');
 
 const currentYear = new Date().getFullYear();
 
-const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+const currentYearTime = new Date(`July 01 ${currentYear + 0} 00:00:00`);
 
 // Update countdown time
 function updateCountdown() {
 	const currentTime = new Date();
-	const diff = newYearTime - currentTime;
+	const diff = currentYearTime - currentTime;
 
 	const d = Math.floor(diff / 1000 / 60 / 60 / 24);
 	const h = Math.floor(diff / 1000 / 60 / 60) % 24;
@@ -24,4 +24,3 @@ function updateCountdown() {
 }
 
 setInterval(updateCountdown, 1000);
-
